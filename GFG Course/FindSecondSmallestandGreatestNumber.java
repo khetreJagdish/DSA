50544fe9f84cd41c7cd3c6a422c4bbf6869a27b3
Jagdishkhetre <30476034+khetreJagdish@users.noreply.github.com>
2024-01-07 00:11:54 +0530

    Update FindSecondSmallestandGreatestNumber.java

--- GFG Course/FindSecondSmallestandGreatestNumber.java
+++ GFG Course/FindSecondSmallestandGreatestNumber.java
@@ -1,1 +1,24 @@
+public class FindSecondSmallestandGreatestNumber{
 
+  public int[] getSeoondLargestandSmallestNumber(int size,int[] a){
+    int smallest = Integer.MAX_VALUE;
+    int largest = Integer.MIN_VALUE;
+
+    int secondSmallest = Integer.MAX_VALUE;
+    int secondLargest = Integer.MIN_VALUE;
+
+    for(int num=0; num<size;num++){
+      smallest = Math.min(smallest,a[num]);
+      largest = Math.max(largest,a[num]);
+    }
+
+    for(int number = 0; number<size;number++){
+      if(){
+        
+      }
+    }
+
+    
+  }
+  
+}
