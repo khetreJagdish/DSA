20672e9df4481790fa0436265abad5cf68737b81
khetreJagdish <30476034+khetreJagdish@users.noreply.github.com>
2024-07-01 20:16:44 +0530

    Commit for stack using LinkedList

--- StriverBeginnerSheet/Stack/MyStackClassUsingLinkedList.java
+++ StriverBeginnerSheet/Stack/MyStackClassUsingLinkedList.java
@@ -2,72 +2,34 @@ public class MyStackClassUsingLinkedList{
 
 	
 	
-	class Node{
-		int data;
-		Node node;
-		
-		Node(int data){
-			this.data = data;
-		}
-	}
-	
-	public MyStackClassUsingLinkedList(){
-		
-		int size = 0;
-		Node head;
-	}
-	
-	//push
-	public Node push(int data){
-		Node newNode = new Node(data);
-		if(head == null){
-			head = newNode;
-		}else{
-			head.next = newNode;
-		}
-	size++;
-	return newNode;
-	}
-	
-	
-	
-	//pop
-	
-	
-	
-	
-	
-	//peek
-	
-	
-	
-	
-	//isFull
-	
-	
-	
-	
-	
-	//isEmpty
-	
-	
-	
-	
-	//size
-	
-	
-	public static void main(String[] args){
-		
-		MyStackClassUsingLinkedList list = new MyStackClassUsingLinkedList();
-		list.push(10);
-		list.push(20);
-		list.push(30);
-		
-		while(list != null){
-			System.out.print(" "+list.data);
-			list = list.next;
-		}
-	
-	}
+	 // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }   
+    StackNode top;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+        // Add your code here
+        StackNode newNode = new StackNode(a);
+        newNode.next = top;
+        top = newNode;
+        
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        // Add your code here
+        if(top == null) return -1;
+        int val = top.data;
+        top = top.next;
+        return val;
+    }
 
 }
