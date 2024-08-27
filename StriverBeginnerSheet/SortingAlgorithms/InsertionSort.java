217213c2d7e122ae9adb4ce211868720d0540d4d
jagdish khetre <jagdish.khetre@gigamon.com>
2024-08-27 11:57:46 +0530

    Commit for Sorting Algorithm Insertion Sort

--- StriverBeginnerSheet/SortingAlgorithms/InsertionSort.java
+++ StriverBeginnerSheet/SortingAlgorithms/InsertionSort.java
@@ -1,24 +1,17 @@
 public class InsertionSort
 {
 
-  public void insertionSort(int arr[], int n)
-  {
-      
-      for(int i = 0; i<n-1;i++){
-          
-          int min = i;
-          
-          for(int j = i; j<n;j++){
-            
-            if(arr[j] < arr[min]){
-                 min = j;
+    public  void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int j = i ;
+            while (j > 0 && arr[j-1] > arr[j]) {
+                int temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				j--;
             }
-          }
-          
-          int temp = arr[min] ;
-          arr[min] = arr[i];
-          arr[i] = temp;
-      }
-      
-  }
+        }
+    }
 }
